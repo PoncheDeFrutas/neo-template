01209a709c0f11ebf289ad5c0c9071c650971bdb
Ponche <josueguevara792@gmail.com>
2025-08-31 11:55:21 -0600

    feat: update HomePage to include button variants for better UI interaction

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,9 +1,21 @@
+import { Button } from '@shared/ui';
 import type { FC } from 'react';
 
 const HomePage: FC = () => {
     return (
         <div>
             <h1>Home Page</h1>
+            <div className="mt-4 flex gap-4">
+                <Button size="sm" variant="primary">
+                    Small
+                </Button>
+                <Button size="md" variant="secondary">
+                    Medium
+                </Button>
+                <Button size="lg" variant="outline">
+                    Large
+                </Button>
+            </div>
         </div>
     );
 };
