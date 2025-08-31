04b327098ded37115bfa0c6c3b8e569c52080902
Ponche <josueguevara792@gmail.com>
2025-08-31 15:10:28 -0600

    refactor: simplify Button component usage in HomePage

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -6,15 +6,9 @@ const HomePage: FC = () => {
         <div>
             <h1>Home Page</h1>
             <div className="mt-4 flex gap-4">
-                <Button size="small" primary={true} label="Small Button" onClick={() => alert('Small Button clicked!')}>
-                    Small
-                </Button>
-                <Button size="medium" primary={false} label="Medium Button" onClick={() => alert('Medium Button clicked!')}>
-                    Medium
-                </Button>
-                <Button size="large" primary={false} label="Large Button" onClick={() => alert('Large Button clicked!')}>
-                    Large
-                </Button>
+                <Button size="small" primary={true} label="Small Button" onClick={() => alert('Small Button clicked!')} />
+                <Button size="medium" primary={false} label="Medium Button" onClick={() => alert('Medium Button clicked!')} />
+                <Button size="large" primary={false} label="Large Button" onClick={() => alert('Large Button clicked!')} />
             </div>
         </div>
     );
